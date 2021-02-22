dde45977f04a3cf88fa2761d873244b0f65e1a12
Piotr Slowik <piotrslowik409@gmail.com>
2021-02-22 15:21:55 +0100

    initial

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -3,40 +3,40 @@ import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp';
 
-
 const SearchScreen = () => {
-    const [term, setTerm] = useState('');
-    const [results, setResults] = useState([]);
-    const [errorMessage, setErrorMessage] = useState('');
-  
+  const [term, setTerm] = useState('');
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const searchApi = async () => {
-    try{
-    const response = await yelp.get('/search', {
-      params: {
-        limit: 50,
-        term,
-        location: 'san jose'
-      }
-    });
-    setResults(response.data.businesses);
-  } catch (err) {
-    setErrorMessage('Something went wrong')
-  }
+  const searchApi = async searchTerm => {
+    console.log('Hi there!');
+    try {
+      const response = await yelp.get('/search', {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: 'san jose'
+        }
+      });
+      setResults(response.data.businesses);
+    } catch (err) {
+      setErrorMessage('Something went wrong ty wacku');
+    }
   };
 
-
+  // Call searchApi when component
+  // is first rendered.  BAD CODE!
+  // searchApi('pasta');
 
   return (
     <View>
-         <SearchBar
+      <SearchBar
         term={term}
-        onTermChange={setTerm} 
-        onTermSubmit={searchApi} />
-        {errorMessage ? <Text>{errorMessage}</Text> : null }
-
-      <Text>Search Screen</Text>
-      <Text>We have found {results.length} results</Text> 
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
