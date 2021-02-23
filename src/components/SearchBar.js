9157835937fca68f98375791655a44ac3016c369
piotr slowik <piotrslowik409@gmail.com>
2021-02-23 13:01:57 +0100

    extract hook logic

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = ({term, onTermChange, onTermSubmit }) => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
       <Feather name="search" style={styles.iconStyle} />
@@ -12,10 +12,8 @@ const SearchBar = ({term, onTermChange, onTermSubmit }) => {
         style={styles.inputStyle}
         placeholder="Search"
         value={term}
-        
         onChangeText={onTermChange}
         onEndEditing={onTermSubmit}
-
       />
     </View>
   );
